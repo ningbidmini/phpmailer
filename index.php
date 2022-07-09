4bbe2f96ec671d50b820ffe456366b98517cbc36
ningbidmini <67812699+ningbidmini@users.noreply.github.com>
2022-07-09 15:06:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ try {
     $mail->Mailer = "smtp";
     //Server settings
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->SMTPDebug = 1;                      //Enable verbose debug output
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
