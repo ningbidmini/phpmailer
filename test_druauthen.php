58e3bab6cb78e96ba48b62bb79c9703123398987
ningbidmini <67812699+ningbidmini@users.noreply.github.com>
2022-07-12 15:48:32 +0700

    Update test_druauthen.php

--- test_druauthen.php
+++ test_druauthen.php
@@ -1,90 +1,34 @@
 <?php
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
-require __DIR__ . '/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
-if (php_sapi_name() != 'cli') {
-    throw new Exception('This application must be run on the command line.');
-}
+// init configuration
+$clientID = '225954121444-jei3gtpeigsn0ch12mr3i7qfd9vfgac2.apps.googleusercontent.com';
+$clientSecret = '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';
+$redirectUri = './test_druauthen.php';
 
-use Google\Client;
-use Google\Service\Gmail;
+// create Client Request to access Google API
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
 
-/**
- * Returns an authorized API client.
- * @return Client the authorized client object
- */
-function getClient()
-{
-    $client = new Client();
-    $client->setApplicationName('Gmail API PHP Quickstart');
-    $client->setScopes('https://www.googleapis.com/auth/gmail.addons.current.message.readonly');
-    $client->setAuthConfig('client_secret_225954121444-jei3gtpeigsn0ch12mr3i7qfd9vfgac2.apps.googleusercontent.com.json');
-    $client->setAccessType('offline');
-    $client->setPrompt('select_account consent');
+// authenticate code from Google OAuth Flow
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
 
-    // Load previously authorized token from a file, if it exists.
-    // The file token.json stores the user's access and refresh tokens, and is
-    // created automatically when the authorization flow completes for the first
-    // time.
-    $tokenPath = 'token.json';
-    if (file_exists($tokenPath)) {
-        $accessToken = json_decode(file_get_contents($tokenPath), true);
-        $client->setAccessToken($accessToken);
-    }
+  // get profile info
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
 
-    // If there is no previous token or it's expired.
-    if ($client->isAccessTokenExpired()) {
-        // Refresh the token if possible, else fetch a new one.
-        if ($client->getRefreshToken()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-        } else {
-            // Request authorization from the user.
-            $authUrl = $client->createAuthUrl();
-            printf("Open the following link in your browser:\n%s\n", $authUrl);
-            print 'Enter verification code: ';
-            $authCode = trim(fgets(STDIN));
-
-            // Exchange authorization code for an access token.
-            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-            $client->setAccessToken($accessToken);
-
-            // Check to see if there was an error.
-            if (array_key_exists('error', $accessToken)) {
-                throw new Exception(join(', ', $accessToken));
-            }
-        }
-        // Save the token to a file.
-        if (!file_exists(dirname($tokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
-        }
-        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-    }
-    return $client;
-}
-
-
-// Get the API client and construct the service object.
-$client = getClient();
-$service = new Gmail($client);
-
-try{
-
-    // Print the labels in the user's account.
-    $user = 'me';
-    $results = $service->users_labels->listUsersLabels($user);
-
-    if (count($results->getLabels()) == 0) {
-        print "No labels found.\n";
-    } else {
-        print "Labels:\n";
-        foreach ($results->getLabels() as $label) {
-            printf("- %s\n", $label->getName());
-        }
-    }
-}
-catch(Exception $e) {
-    // TODO(developer) - handle error appropriately
-    echo 'Message: ' .$e->getMessage();
+  // now you can use this profile info to create account in your website and make user logged in.
+} else {
+  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
 }
 ?>
