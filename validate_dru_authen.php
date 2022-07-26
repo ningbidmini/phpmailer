45350474eca26a702524aefd954ec6c1fe1a4ee0
ningbidmini <67812699+ningbidmini@users.noreply.github.com>
2022-07-26 14:56:20 +0700

    Update validate_dru_authen.php

--- validate_dru_authen.php
+++ validate_dru_authen.php
@@ -21,31 +21,39 @@ if(count($token)>0){
   $error_response = curl_exec($ch);
   curl_close ($ch);
 
-  var_dump($error_response);
+  // var_dump($error_response);
   $array = json_decode($error_response);
-  echo "<br/>";
-  var_dump($array);
-
-  echo json_encode($status);
+  // echo "<br/>";
+  // var_dump($array);
+  //
+  // echo json_encode($status);
 }
 
-// if( isset($array->error)){
-//
-//   // Generate new Access Token using old Refresh Token
-//   $ch = curl_init();
-//   curl_setopt($ch, CURLOPT_URL,"https://accounts.google.com/o/oauth2/token");
-//   curl_setopt($ch, CURLOPT_POST, TRUE);
-//   curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
-//   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-//   curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-//     'client_id'     => $client_id,
-//     'client_secret' => $client_secret,
-//     'refresh_token'  => $refresh_token,
-//     'grant_type'    => 'refresh_token',
-//   ]));
-//   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//   $response = curl_exec($ch);
-//   curl_close ($ch);
-// }
+if( isset($array->error)){
 
+  // Generate new Access Token using old Refresh Token
+  // $ch = curl_init();
+  // curl_setopt($ch, CURLOPT_URL,"https://accounts.google.com/o/oauth2/token");
+  // curl_setopt($ch, CURLOPT_POST, TRUE);
+  // curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
+  // curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+  //   'client_id'     => $client_id,
+  //   'client_secret' => $client_secret,
+  //   'refresh_token'  => $refresh_token,
+  //   'grant_type'    => 'refresh_token',
+  // ]));
+  // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  // $response = curl_exec($ch);
+  // curl_close ($ch);
+  foreach ($array as $key => $value) {
+    $status['error'][$key]=$value;
+  }
+}else{
+  $status['status']=true;
+  foreach ($array as $key => $value) {
+    $status['data'][$key]=$value;
+  }
+}
+echo json_encode($status);
 ?>
