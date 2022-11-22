1feb904f882a50aafe8b68c5d067935bd529e8df
ningbidmini <67812699+ningbidmini@users.noreply.github.com>
2022-11-22 13:48:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,8 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     // $mail->SMTPSecure = "tls";
     $mail->Username   = 'tossapol.c@dru.ac.th';                     //SMTP username
-    $mail->Password   = 'zfdzapsgrugwvtbg';                               //SMTP password
+    //$mail->Password   = 'zfdzapsgrugwvtbg';                               //SMTP password
+    $mail->Password = 'swrtbddjzjftkdts';
     // $mail->Password   = 'vovhgktagykraidr';                               //SMTP password
     // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
